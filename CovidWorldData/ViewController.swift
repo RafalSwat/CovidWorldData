9a5e40e4bdbdda89dddddd56cc4d6b50e52ec9e7
RafalSwat <rafal.swat@gmail.com>
2021-04-12 14:16:57 +0200

    Initial Commit

---
+++ CovidWorldData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CovidWorldData
+//
+//  Created by Rafał Swat on 12/04/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
